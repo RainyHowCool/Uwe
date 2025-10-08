af50a8dc708a98c926e7afa55a090d73c4e21f95
RainyHowCool <rainyhowcool@outlook.com>
2025-10-08 14:17:46 +0800

    更加优雅的IR处理方式

--- src/ir.cpp
+++ src/ir.cpp
@@ -10,29 +10,24 @@
 #include <format>
 #include <cstdint>
 
-
 import lexer;
 import parser;
 import logger;
 import vm;
 
-/*
-#define MatchResult std::vector<std::pair<std::string, std::string>>
-typedef std::vector<std::pair<std::regex*, std::string>> LexerRule;
-typedef std::vector<std::pair<std::function<int(MatchResult)>, std::vector<std::string>>> ParserRule;
-*/
-
 #define LRULE(x, y) LexerRule(std::regex(x), y)
-#define RRULE ParserRule
 
-using namespace std;
+// 节点树
+std::vector<Node*> Nodes;
 
-int generate_ir(std::string code, char** mem)
+// 程序计数器
+uint32_t pc;
+
+// 内存
+char** mem = nullptr;
+
+static std::list<LexerRule> getLexerRules()
 {
-	// 节点树
-	std::vector<Node*> Nodes;
-
-	// IR 的 Lexer 规则
 	std::list<LexerRule> matches =
 	{
 		LRULE(R"(\n)", "EOL"),
@@ -41,71 +36,89 @@ int generate_ir(std::string code, char** mem)
 		LRULE(R"([a-zA-Z_][a-zA-Z0-9_]*)", "IDENTIFIER"),
 		LRULE(R"(,)", "COMMA"),
 	};
+	return matches;
+}
 
-	// 程序计数器
-	uint32_t pc = 24;
+auto trigger1 = [](LexerResult result) -> Option<int> {
+	Node* node = nullptr;
+	std::string keyword(result.get(0).result.c_str());
+	std::string para1(result.get(1).result.c_str());
+	std::string para2(result.get(3).result.c_str());
+	if (keyword == "add")
+		node = new MathOpNode(para1, '+', atoi(para2.c_str()));
+	else if (keyword == "sub")
+		node = new MathOpNode(para1, '-', atoi(para2.c_str()));
+	else if (keyword == "mul")
+		node = new MathOpNode(para1, '*', atoi(para2.c_str()));
+	else if (keyword == "div")
+		node = new MathOpNode(para1, '/', atoi(para2.c_str()));
+	else if (keyword == "mov")
+		node = new MoveNode(para1, para2);
+	else
+		Log.fatal("Bad command");
+	Nodes.push_back(node);
+	return Option<int>(true, 0);
+};
 
+auto trigger2 = [](LexerResult result) -> Option<int> {
+	Node* node = nullptr;
+	std::string keyword(result.get(0).result.c_str());
+	std::string para1(result.get(1).result.c_str());
+	if (keyword == "push")
+		node = new PushNode(para1);
+	else if (keyword == "pop")
+		node = new PopNode(para1);
+	else if (keyword == "dat") {
+		node = new DataNode(para1);
+		pc += node->codegen(*mem + pc);
+		return Option<int>(true, 0);
+	}
+	else
+		Log.fatal("Bad command");
+	Nodes.push_back(node);
+	return Option<int>(true, 0);
+};
+
+auto trigger3 = [](LexerResult result) -> Option<int> {
+	Node* node = nullptr;
+	std::string keyword(result.get(0).result.c_str());
+	if (keyword == "quit")
+		node = new QuitNode();
+	else if (keyword == "invoke")
+		node = new InvokeNode();
+	else
+		Log.fatal("Bad command");
+	Nodes.push_back(node);
+	return Option<int>(true, 0);
+};
+
+static std::list<ParserRule> getASTRules()
+{
 	// IR 的 AST 规则
 	std::list<ParserRule> rules =
 	{
-		ParserRule([&Nodes](LexerResult result) -> Option<int> {
-			// 节点
-			Node* node;
-			std::string keyword(result.get(0).result.c_str());
-			std::string para1(result.get(1).result.c_str());
-			std::string para2(result.get(3).result.c_str());
-			// 检查 keyword
-			if (keyword == "add")
-				node = dynamic_cast<Node*>(new MathOpNode(para1, '+', atoi(para2.c_str())));
-			else if (keyword == "sub")
-				node = dynamic_cast<Node*>(new MathOpNode(para1, '-', atoi(para2.c_str())));
-			else if (keyword == "mul")
-				node = dynamic_cast<Node*>(new MathOpNode(para1, '*', atoi(para2.c_str())));
-			else if (keyword == "div")
-				node = dynamic_cast<Node*>(new MathOpNode(para1, '/', atoi(para2.c_str())));
-			else if (keyword == "mov")
-				node = dynamic_cast<Node*>(new MoveNode(para1, para2));
-			else
-				Log.fatal("Bad command");
-
-			Nodes.push_back(node);
-			return Option<int>(true, 0);
-		}, { "IDENTIFIER", "IDENTIFIER", "COMMA", "IDENTIFIER" }),
-	ParserRule([&Nodes,&mem, &pc](LexerResult result) -> Option<int> {
-			// 节点
-				Node* node;
-				std::string keyword(result.get(0).result.c_str());
-				std::string para1(result.get(1).result.c_str());
-				// 检查 keyword
-				if (keyword == "push")
-					node = dynamic_cast<Node*>(new PushNode(para1));
-				else if (keyword == "pop")
-					node = dynamic_cast<Node*>(new PopNode(para1));
-				else if (keyword == "dat") {
-					node = dynamic_cast<Node*>(new DataNode(para1));
-					pc += node->codegen(*mem + pc);
-					return Option<int>(true, 0);
-				}
-				else
-					Log.fatal("Bad command");
-				Nodes.push_back(node);
-				return Option<int>(true, 0);
-		}, { "IDENTIFIER", "IDENTIFIER" }),
-	ParserRule([&Nodes](LexerResult result) -> Option<int> {
-			// Node
-			Node* node;
-			std::string keyword(result.get(0).result.c_str());
-			// 检查 keyword
-			if (keyword == "quit")
-				node = dynamic_cast<Node*>(new QuitNode());
-			else if (keyword == "invoke")
-				node = dynamic_cast<Node*>(new InvokeNode());
-			else
-				Log.fatal("Bad command");
-			Nodes.push_back(node);
-			return Option<int>(true, 0);
-		}, { "IDENTIFIER" })
+		ParserRule(trigger1, { "IDENTIFIER", "IDENTIFIER", "COMMA", "IDENTIFIER" }),
+		ParserRule(trigger2, { "IDENTIFIER", "IDENTIFIER" }),
+		ParserRule(trigger3, { "IDENTIFIER" })
 	};
+
+	return rules;
+}
+
+int generate_ir(std::string code, char** memory)
+{
+	// 初始化变量
+	Nodes.clear();
+	pc = 24;
+
+	// 设置内存
+	mem = memory;
+
+	// IR 的 Lexer 规则
+	auto matches = getLexerRules();
+
+	// IR 的 AST 规则
+	auto rules = getASTRules();
 
 	// 运行词法解析器
 	Lexer lexer(code, matches);
